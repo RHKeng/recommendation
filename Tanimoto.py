6bcd5fab763eea356ba233876aefdef9d264202c
RHKeng <458271503@qq.com>
2016-06-04 02:10:54 -0700

    Tanimoto

---
+++ Tanimoto.py
@@ -0,0 +1,19 @@
+from __future__ import division
+
+#return Tanimoto
+def Tanimoto_distance(prefs,person1,person2):
+    #get the same items of person1 and person2
+    si={}
+    tanimoto=0
+    for item in prefs[person1]:
+        if item in prefs[person2]:
+            si[item]=1
+            if prefs[person1][item]==prefs[person2][item]:
+                tanimoto+=1
+                print tanimoto
+
+    #none same items
+    if len(si)==0:
+        return 0
+    else:
+        return tanimoto/(len(si)*2)
--- debug.py
+++ debug.py
@@ -3,22 +3,26 @@ from recommendations import sim_distance
 from recommendations import sim_pearson
 from recommendations import topMatches
 from recommendations import getRecommendations
+from Tanimoto import Tanimoto_distance
 
-print sim_distance(critics,'Lisa Rose','Gene Seymour')
-print sim_pearson(critics,'Lisa Rose','Gene Seymour')
-print topMatches(critics,'Toby',n=3)
-print getRecommendations(critics,'Toby')
-
-from recommendations import transformPrefs
-moives=transformPrefs(critics)
-print topMatches(moives,'Superman Returns')
-print getRecommendations(moives,'Just My Luck')
-
+# print sim_distance(critics,'Lisa Rose','Gene Seymour')
+# print sim_pearson(critics,'Lisa Rose','Gene Seymour')
+# print topMatches(critics,'Toby',n=3)
+# print getRecommendations(critics,'Toby')
+#
+# from recommendations import transformPrefs
+# moives=transformPrefs(critics)
+# print topMatches(moives,'Superman Returns')
+# print getRecommendations(moives,'Just My Luck')
+#
+# # import pydelicious
+# from PIL import Image,ImageDraw
+# from bs4 import BeautifulStoneSoup
+# from pysqlite2 import dump
+# from numpy import *
+# from pylab import *
 # import pydelicious
-from PIL import Image,ImageDraw
-from bs4 import BeautifulStoneSoup
-from pysqlite2 import dump
-from numpy import *
-from pylab import *
-import pydelicious
+#
+# print pydelicious.get_popular(tag='programming')
 
+print Tanimoto_distance(critics,'Lisa Rose','Gene Seymour')
